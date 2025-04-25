17fede344eb26dc4af3a1680be5181346d927c2e
GoodBoy119lab <sandinogoodboy@gmail.com>
2025-04-25 19:09:54 +0200

    SGEMP

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/*index.html',
+    './src/*clientes.html',
+    './src/*vendas.html',
+    './src/*produtos.html',
+    'styles.css',
+  ],
+  theme: {
+    extend: {
+      colors: {
+ 
+      },
+    },
+  },
+  plugins: [],
+}
+
